74112ca61fdee0498f8430d7029845d87ef7446a
Placer0884 <Erikpersson0884@gmail.com>
2022-02-25 10:18:05 +0100

    removed test.py

--- OS-gren efter Flowchart/test.py
+++
@@ -1,8 +0,0 @@
-yes = ["Yes", "yes", "Y", "y", "Yes!", "yes!", "Ja", "ja"]
-no = ["No", "no", "N", "n", "No!", "no!", "Nej", "nej"]
-stop = ["Stop", "stop", "Stop!", "stop!"]
-cancel = ["Cancel", "cancel"]
-
-lists = [yes[:], no[:], stop[:], cancel[:]]
-if input(">") in lists:
-    print("YES")
